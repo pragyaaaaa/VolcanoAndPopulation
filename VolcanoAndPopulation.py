fbce4aea568e0a5d374152ccec1342dbad03a29d
pragyaaaaa <pragyasabstracts@gmail.com>
2020-10-15 20:24:09 +0530

    Basic map generation using folium: feature group added.

--- VolcanoAndPopulation.py
+++ VolcanoAndPopulation.py
@@ -1,4 +1,7 @@
 import folium
 
 map_f = folium.Map(location=[54.5260, -105.2551], zoom_start=3, tiles="Stamen Terrain")
+feature_group = folium.FeatureGroup(name="Volcano and Population")
+feature_group.add_child(folium.Marker(location=[37.0902, -95.7129], popup="USA", icon=folium.Icon(color="red")))
+map_f.add_child(feature_group)
 map_f.save("volcanoandmap.html")
